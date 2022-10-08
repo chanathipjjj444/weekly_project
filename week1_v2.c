8729734e5ab4eee46462b405c39ae2cfb29a2ddf
Jay <107759970+chanathipjjj444@users.noreply.github.com>
2022-10-08 11:08:12 +0700

    Update week1_v2.c
    เเก้ weeky project week1_v1

--- week1_v2.c
+++ week1_v2.c
@@ -3,27 +3,45 @@ int main(){
     int score;
     printf("Enter your score: ");
     scanf("%d", &score);
+    if(score >= 0 && score <= 100){
+        switch (score/10)
+        {
+        case 10:
+            printf("You got grade A!!");
+            break;
+        case 9:
+            printf("You got grade A!!");
+            break;
+        case 8:
+            printf("You got grade B");
+            break;
+        case 7:
+            printf("You got grade C");
+            break;
+        case 6:
+            printf("You got grade D"); 
+            break;
+        case 5:
+        printf("You got grade F"); 
+        break;
+            case 4:
+        printf("You got grade F"); 
+        break;
+            case 3:
+        printf("You got grade F"); 
+        break;
+            case 2:
+        printf("You got grade F"); 
+        break;
+            case 1:
+        printf("You got grade F"); 
+        break;
+            case 0:
+        printf("You got grade F"); 
+        break;
 
-    switch (score/10)
-    {
-    case 10:
-        printf("You got grade A!!");
-        break;
-    case 9:
-        printf("You got grade A!!");
-
-        break;
-    case 8:
-        printf("You got grade B");
-        break;
-    case 7:
-        printf("You got grade C");
-        break;
-    case 6:
-        printf("You got grade D"); 
-        break;
-    default:
-        printf("You got grade F");
-        break;
-    }
+        }
+        }
+    else
+        printf("Please enter a valid value between 0-100");
 }
