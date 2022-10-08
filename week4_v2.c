17d15153034e36f7172ac16e3bf127eccad8c98b
Jay <107759970+chanathipjjj444@users.noreply.github.com>
2022-10-08 11:27:52 +0700

    Update week4_v2.c
    อัพโหลดใหม่ week4_v2

--- week4_v2.c
+++ week4_v2.c
@@ -2,7 +2,7 @@
 int main(){
     int n,i,j;
     scanf("%d", &n);
-    for(i=1; i<n; i++){
+    for(i=0; i<n; i++){
         for(j=1;j<=(2*n)-1; j++){
             if(j <= i || j>= 2*n-i || i==n){
                 printf(" ");
@@ -13,7 +13,7 @@ int main(){
         }
         printf("\n");
     }
-    for(i=2;i<=n-1;i++){
+    for(i=2;i<=n;i++){
         for(j=1;j<= 2*n-1;j++){
             if (j<= n-i || j >= n+i)
             {
